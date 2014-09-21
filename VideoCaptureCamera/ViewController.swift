08a8004ab2f6b896f49460b398b49cf5b30862d8
nobuhito muto <nobuhito.muto@gmail.com>
2014-09-22 02:34:22 +0900

    Initial Commit

---
+++ VideoCaptureCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoCaptureCamera
+//
+//  Created by Muto Nobuhito on 2014/09/22.
+//  Copyright (c) 2014年 Muto Nobuhito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
